3553e1a873be2c972d8bf85e7f5834fb3f13f7b3
Zainab 27 <zainabalsaffar03@gmail.com>
2025-02-16 17:57:58 +0300

    Part 1

---
+++ ClassesTask/src/WithoutConstructor.kt
@@ -0,0 +1,11 @@
+class Book {
+    var title: String = ""
+    var author: String = ""
+    var yearPublished: Int = 0
+}
+fun main (){
+    var newBook =Book()
+    newBook.title = "To kill a mockingbird"
+    newBook.author = "Harper Lee"
+    newBook.yearPublished = 1960
+}
