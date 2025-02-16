fdb34974827824a76d49ded502e60ba40dafd611
Zainab 27 <zainabalsaffar03@gmail.com>
2025-02-16 18:40:09 +0300

    All 3 parts of the classes exercise

--- ClassesTask/src/WithoutConstructor.kt
+++ ClassesTask/src/WithoutConstructor.kt
@@ -1,11 +1,37 @@
-class Book {
-    var title: String = ""
-    var author: String = ""
-    var yearPublished: Int = 0
+class Book(val title: String, val author: String, val yearPublished: Int) {
+    fun getBookInfo(): String {
+        return "$title by $author ($yearPublished)"
+    }
 }
-fun main (){
-    var newBook =Book()
-    newBook.title = "To kill a mockingbird"
-    newBook.author = "Harper Lee"
-    newBook.yearPublished = 1960
+
+fun main() {
+    val newBook = Book("To Kill a Mockingbird", "Harper Lee", 1960)
+    println(newBook.getBookInfo())
 }
+
+
+//class Book {
+//    var title: String = ""
+//    var author: String = ""
+//    var yearPublished: Int = 0
+//}
+//fun main (){
+//    var newBook =Book()
+//    newBook.title = "To kill a mockingbird"
+//    newBook.author = "Harper Lee"
+//    newBook.yearPublished = 1960
+//}
+//
+//class Book(val title: String, val author: String, val yearPublished: Int) {
+//    fun printDetails() {
+//        println("Book Title: $title")
+//        println("Author: $author")
+//        println("Year Published: $yearPublished")
+//    }
+//}
+//
+//fun main() {
+//    val newBook = Book("To Kill a Mockingbird", "Harper Lee", 1960)
+//    newBook.printDetails()
+//}
+
